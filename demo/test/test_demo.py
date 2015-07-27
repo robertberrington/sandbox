dc71b3a76a61fe6a080caaac4715dece0f89f0f1
Robert Berrington <robertberringtoniii@gmail.com>
2015-07-26 22:29:28 -0400

    adding other files not copied by windows cmd

---
+++ demo/test/test_demo.py
@@ -0,0 +1,32 @@
+"""Sample unit test module."""
+
+import unittest
+
+from demo import sample
+
+
+class TestDemo(unittest.TestCase):
+
+    """Sample unit test class."""
+
+    def test_dependency_import(self):
+        """Sample test method for dependencies."""
+        try:
+            import testpackage  # pylint: disable=unused-variable
+            assert True
+        except ImportError:
+            self.fail("dependency not installed")
+
+    def test_dependency_import_special(self):
+        """Sample test method for special dependencies."""
+        try:
+            import newrelic_plugin_agent  # pylint: disable=unused-variable
+            assert True
+        except ImportError:
+            self.fail("dependency not installed")
+
+    def test_branch_coverage(self):
+        """Sample test method for branch coverage."""
+        self.assertEquals(sample.function(True), 'True')
+        self.assertEquals(sample.function(False), 'False')
+        self.assertEquals(sample.function(None), 'None')
